fb140de84b5a61201a8e01bd9571c7213b2f8b6a
@湘猿 <412277685@qq.com>
2016-10-22 17:05:48 +0800

    Update main.js
    blank

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,10 @@ function retina() {
 };
  
 $(document).ready(retina);
+
+$(document).ready(function() {
+	    //为超链接加上target='_blank'属性
+		$('a[href^="http"]').each(function() {
+			$(this).attr('target', '_blank');
+		});
+	});
