660a617ce9f74bd5df1058d9d1608fc48e3f3410
@湘猿 <412277685@qq.com>
2016-10-22 17:13:15 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ $(document).ready(retina);
 
 $(document).ready(function() {
 	    //为超链接加上target='_blank'属性
-		$('a[href^="http"]').each(function() {
+		$('a').each(function() {
 			$(this).attr('target', '_blank');
 		});
 	});
